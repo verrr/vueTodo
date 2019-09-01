161d495a466f83100dcb56c9085fa5636c8ba4e2
Andrei Verdes <ave@mir-robots.com>
2019-09-01 18:55:50 +0200

    basic crud and state management implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddTodo/>
+    <Todos/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue";
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo
+  },
+  data(){
+    return{
+      msg: 'hi'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>
+            Todos
+        </h3>
+        <div class="todos">
+            <div v-for='todo in allTodos' v-bind:key='todo.id' class="todo">
+                {{todo.title}}
+                <input type="button" value="Remove" @click="deleteTodo(todo.id)"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex';
+
+export default {
+    name: 'Todos',
+    methods:{
+        ...mapActions(['fetchTodos', 'deleteTodo'])
+    },
+    computed: mapGetters(['allTodos']),
+    created(){
+        this.fetchTodos();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
